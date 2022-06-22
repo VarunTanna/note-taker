b948a192fbae3e68c9272fe88658caace4cb73ae
Varun Tanna <varun_tanna@ymail.com>
2022-06-22 14:35:23 -0400

    added static

--- server.js
+++ server.js
@@ -10,4 +10,7 @@ const PORT = process.env.PORT || 3001;
 // bringing back data as json
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
+
+
 
