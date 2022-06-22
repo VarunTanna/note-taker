57d2a1820156fef9f433c85de29bf5a0344bfae4
Varun Tanna <varun_tanna@ymail.com>
2022-06-22 09:54:38 -0400

    starting server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+// dependencies 
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+
+// initialize the express, set inital port for listener
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// bringing back data as json
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
