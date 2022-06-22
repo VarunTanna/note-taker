934772b7317b28ea9e5a737eb5fccd5dd9791411
Varun Tanna <varun_tanna@ymail.com>
2022-06-22 14:53:34 -0400

    set up our get's

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // dependencies 
 const express = require("express");
+const res = require("express/lib/response");
 const fs = require("fs");
 const path = require("path");
 
@@ -11,6 +12,19 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "public/notes.html"));
+});
+
+app.get("/api/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "db.json"));
+});
+
 
 
 
